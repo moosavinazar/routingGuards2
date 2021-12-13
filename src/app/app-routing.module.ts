2a4a7a05ec1f97f7b04d9173c979bb56947652da
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-13 20:20:35 +0330

    create project with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {AdminComponent} from "./admin/admin.component";
+import {NotAuthorizedComponent} from "./not-authorized/not-authorized.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'not-authorized', component: NotAuthorizedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { ManagePagesComponent } from './manage-pages/manage-pages.component';
+import { ManageUsersComponent } from './manage-users/manage-users.component';
+import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AdminComponent,
+    HomeComponent,
+    ManagePagesComponent,
+    ManageUsersComponent,
+    NotAuthorizedComponent
   ],
   imports: [
     BrowserModule,
